0ec7f25b89bbd8f2ce9863945100e5a30a0808f0
Igor Wolf <igor.wolf99@gmail.com>
2023-05-23 23:19:14 -0300

    Entidade Produto, ORM

--- src/main/java/com/igorwolf/pdlist/PdlistApplication.java
+++ src/main/java/com/igorwolf/pdlist/PdlistApplication.java
@@ -3,11 +3,13 @@ package com.igorwolf.pdlist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class PdlistApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PdlistApplication.class, args);
 	}
-
+	
 }
---
+++ src/main/java/com/igorwolf/pdlist/entities/Product.java
@@ -0,0 +1,55 @@
+package com.igorwolf.pdlist.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_product")
+public class Product implements Serializable{
+	
+	private static long serialVersionID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long Id;
+	
+	private String nome;
+	private Integer quantidade;
+	private Double	valor;
+	
+	
+	
+	
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	
+	
+	
+}
