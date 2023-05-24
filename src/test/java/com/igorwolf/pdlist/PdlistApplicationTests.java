30d42221c10825aa81d0942393d4efccd72cd325
Igor Wolf <igor.wolf99@gmail.com>
2023-05-23 22:41:27 -0300

    Projeto Criado

---
+++ src/test/java/com/igorwolf/pdlist/PdlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorwolf.pdlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
